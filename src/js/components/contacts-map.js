7251934934645ccc8ea1dd2a1e7a13597c0abdb4
crysknife <ad.dolorem@yandex.ru>
2017-10-22 00:13:20 +0300

    Fixed locations.html no padding in locations list

--- src/js/components/contacts-map.js
+++ src/js/components/contacts-map.js
@@ -4,8 +4,12 @@ $(document).ready(() => {
 	if ($('.astb-contacts').length > 0) {
 		$script('https://maps.googleapis.com/maps/api/js?key=AIzaSyCWgWiXe4fdVDjYj6-APt80DhtH2o05w8U&amp', function() {
 
+			let mapEl = $('.aste-map');
+			let lat = +mapEl.attr('data-lat');
+			let lng = +mapEl.attr('data-lng');
+
 			let contactsMap = new google.maps.Map(document.querySelector('.aste-map'), {
-				center: { lat: 55.811600, lng: 37.456668 },
+				center: { lat, lng },
 				zoom: 14,
 				disableDefaultUI: true
 			});
